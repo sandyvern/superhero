cff56f50fc5de6e0544ce537cdf0818e8285baef
Sandy Hogan <SVern99@aol.com>
2017-09-22 21:22:36 +0000

    Added faker gem

--- Gemfile
+++ Gemfile
@@ -12,6 +12,7 @@ gem 'jbuilder', '~> 2.0'
 gem 'sdoc', '~> 0.4.0', group: :doc
 
 gem 'bootstrap-sass', '~> 3.3', '>= 3.3.7'
+gem 'faker', '~> 1.8', '>= 1.8.4'
 
 group :development, :test do
   gem 'byebug'
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,12 @@
 class HomeController < ApplicationController
+  before_action :common, :only => [:index, :team]
+  
+  def common
+    @hero_name = Faker::Superhero.name
+    @hero_power = Faker::Superhero.power
+    @hero_prefix = Faker::Superhero.prefix
+  end
+  
   def index
   end
   
